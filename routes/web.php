c7124ecb167a9c052ce9c9d95e02eb055128cebc
hys793146464 <793146464@qq.com>
2019-09-12 08:50:04 +0000

    Finish? login and logout

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 //注册路由时，URI signup 和 /signup 从使用上来看，并无区别，Laravel 框架兼容这两种写法。
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users','UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
